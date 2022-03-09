605d5db29fef9224533d2da4eca84abe08245708
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-09 22:19:10 +0500

    bootstrap and materlized css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
-import LandingPage from './Modules or Pages/LandingPage';
+import "./App.css";
+import LandingPage from "./Modules or Pages/LandingPage";
+import "bootstrap/css/bootstrap.min.css";
+// if import does not contain relative path syntax it will auto find the folder from node modules
 
 function App() {
   return (
     <div className="App">
-      <LandingPage></LandingPage>
-
+      <LandingPage />
     </div>
   );
 }
