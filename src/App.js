da6553113ba6fc76a1a29adeb7c5781f761743f7
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-09 22:35:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import LandingPage from "./Modules or Pages/LandingPage";
-import "bootstrap/css/bootstrap.min.css";
+// import "bootstrap/css/bootstrap.min.css";
+// import "materialize/css/materialize.min.css";
+
 // if import does not contain relative path syntax it will auto find the folder from node modules
 
 function App() {
